be22d4874d3b559b97464dcc881c9f5e250b91ad
BrunoPolaski <polaskibruno03@gmail.com>
2025-11-06 09:34:14 -0300

    feat: adding more error types

--- rest_err/rest_err.go
+++ rest_err/rest_err.go
@@ -75,3 +75,117 @@ func NewUnauthorizedError(message string) *RestErr {
 		Code:    http.StatusUnauthorized,
 	}
 }
+
+func NewBadGatewayError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad gateway",
+		Code:    http.StatusBadGateway,
+	}
+}
+
+func NewConflictError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "conflict",
+		Code:    http.StatusConflict,
+	}
+}
+
+func NewUnprocessableEntityError(message string, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "unprocessable entity",
+		Code:    http.StatusUnprocessableEntity,
+		Causes:  causes,
+	}
+}
+
+func NewTooManyRequestsError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "too many requests",
+		Code:    http.StatusTooManyRequests,
+	}
+}
+
+func NewServiceUnavailableError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "service unavailable",
+		Code:    http.StatusServiceUnavailable,
+	}
+}
+
+func NewGatewayTimeoutError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "gateway timeout",
+		Code:    http.StatusGatewayTimeout,
+	}
+}
+
+func NewPreconditionFailedError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "precondition failed",
+		Code:    http.StatusPreconditionFailed,
+	}
+}
+
+func NewNotAcceptableError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "not acceptable",
+		Code:    http.StatusNotAcceptable,
+	}
+}
+
+func NewLengthRequiredError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "length required",
+		Code:    http.StatusLengthRequired,
+	}
+}
+
+func NewUnsupportedMediaTypeError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "unsupported media type",
+		Code:    http.StatusUnsupportedMediaType,
+	}
+}
+
+func NewExpectationFailedError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "expectation failed",
+		Code:    http.StatusExpectationFailed,
+	}
+}
+
+func NewConflictValidationError(message string, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "conflict",
+		Code:    http.StatusConflict,
+		Causes:  causes,
+	}
+}
+
+func NewRequestTimeoutError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "request timeout",
+		Code:    http.StatusRequestTimeout,
+	}
+}
+
+func NewHttpVersionNotSupportedError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "http version not supported",
+		Code:    http.StatusHTTPVersionNotSupported,
+	}
+}
